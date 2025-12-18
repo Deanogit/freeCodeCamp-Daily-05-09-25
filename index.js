0cbcf9043db02497fa67288d1056f93ca9af299c
Dean Howe <hideanhowe@gmail.com>
2025-12-18 19:50:22 +0000

    solution

--- index.js
+++ index.js
@@ -11,7 +11,52 @@ function isValidIPv4(ipv4) {
   // NO leading 0s (!01)
   // only numeric
   // regex
-  const regex = /[0-255+]\.[0-255+]\.[0-255+]\.[0-255+]/;
-  console.log(regex.test(ipv4));
-  return ipv4;
+  // regex not so good for numeric!
+  // const regex = /[0-255+]\.[0-255+]\.[0-255+]\.[0-255+]/;
+  // console.log(regex.test(ipv4));
+
+  // better to split into arr
+  const arr = ipv4.split('.');
+  if (arr.length !== 4) {
+    return false;
+  }
+
+  console.log(arr);
+
+  function numChecker(num) {
+    const regex = /^\d+$/;
+    if (regex.test(num)) {
+      const value = Number(num);
+      // console.log(number)
+      return value >= 0 && value <= 255;
+    }
+    return false;
+  }
+
+  function strChecker(str) {
+    if (str.length > 1 && str.startsWith('0')) {
+      return false;
+    }
+    return true;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!strChecker(arr[i]) || !numChecker(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+
+  //arr.forEach((x) => {
+  //  console.log(arr[x])
+
+  //  }
+  // }
+  // )
+
+  console.log(strChecker(arr[0]));
+
+  console.log(numChecker(arr[0]));
+
+  // return ipv4;
 }
